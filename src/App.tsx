605e6453549af5e5f7138e65532b2273130bf55f
Maxime Evangeliste <maxime@miloguide.com>
2022-08-30 10:25:45 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="p-5 text-center flex-col">
+      <h1 className="text-2xl font-bold">Miloguide TODO app</h1>
+    </div>
+  );
+}
+
+export default App;
