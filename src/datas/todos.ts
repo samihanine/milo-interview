874e46c0c4db01b2487854fb740fee8bed4eacee
Sam <64200816+samihanine@users.noreply.github.com>
2022-09-17 17:08:49 -0400

    feat: adding example data

--- src/datas/todos.ts
+++ src/datas/todos.ts
@@ -2,14 +2,42 @@ import tags from './tags';
 import { Todo } from '../interfaces/Todo';
 import { getStorage } from '../utils/util';
 
-let todos: Todo[] = [
+const todos: Todo[] = getStorage('todos') || [
   {
-    title: 'Work',
+    title: 'Apprendre Vue.js',
+    isCompleted: false,
+    tag: tags[2],
+  },
+  {
+    title: 'Faire mes exercices de chimie',
+    isCompleted: true,
+    tag: tags[2],
+  },
+  {
+    title: 'Trouver un travail à temps partiel',
+    isCompleted: true,
+    tag: tags[0],
+  },
+  {
+    title: 'Apprendre Capacitor',
     isCompleted: false,
     tag: tags[0],
   },
+  {
+    title: 'Nourrir mon hamster',
+    isCompleted: false,
+    tag: tags[1],
+  },
+  {
+    title: 'Nourrir mon chat',
+    isCompleted: false,
+    tag: tags[1],
+  },
+  {
+    title: 'Regarder la fin de Westworld',
+    isCompleted: true,
+    tag: tags[1],
+  },
 ];
-
-todos = getStorage('todos') || todos;
 
 export default todos;
